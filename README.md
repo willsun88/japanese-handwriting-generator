461411d281866cadd6cd22449e74ac79defcdd64
William Sun <william.h.sun.88@gmail.com>
2021-12-22 19:12:11 -0500

    First round data processing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Japanese Handwriting Generator
 A neural net to convert Japanese kanji into handwritten-style digits
+
+## How to use
+Tested on Python 3.8
+1. Get access to the ETL Character Database, and download ETL9.
+2. Use the [ETLCDB Image Extractor](https://github.com/choo/etlcdb-image-extractor) to extract the images, then place them in the data/ folder in this repository.
+3. Download the Takao Gothic font, and place the TakaoGothic.tff file in the root directory of this repository.
+4. Run `python add_starting_images.py` to generate the font images.
---
+++ add_starting_imgs.py
@@ -0,0 +1,30 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+import os
+
+# Our data
+dataset = "ETL9G"
+
+# Get curr path
+absPath, _ = os.path.split(os.path.abspath(__file__))
+
+# Function to generate image from kanji/kana
+def gen_image_from_char(ch, filename):
+    im = Image.new('RGB', (128, 127), color=(255, 255, 255))
+    drawer = ImageDraw.Draw(im)
+    y_ran = range(18, 33)
+    x_ran = range(23, 38)
+    drawer.text((random.choice(x_ran), random.choice(y_ran)), ch, 
+                font=ImageFont.truetype('TakaoGothic.ttf', 70), fill=(0, 0, 0))
+    im.save(filename, "png")
+
+# Get all subfolders of our data
+sub_folders = []
+for dir, sub_dirs, files in os.walk(absPath+"/data/" + dataset):
+    sub_folders.extend(sub_dirs)
+
+# Generate and save all the text images (true label)
+for folder in sub_folders:
+    with open(absPath + "/data/" + dataset + "/" + folder + "/.char.txt") as f:
+        a = f.readlines()
+    gen_image_from_char(a[0], absPath + "/data/" + dataset + "/" + folder + "/true.png")
